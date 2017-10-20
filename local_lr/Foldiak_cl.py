8e7998d23bbe92857021cc4afa8f4bc98e8a2d1f
Shuang <lishuang2009@gmail.com>
2017-10-20 16:34:12 -0500

    Tested QBCM rule with variable version of foldiak rule

--- local_lr/Foldiak_cl.py
+++ local_lr/Foldiak_cl.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from scipy.special import expit
 import pdb
 
+
 # Define Foldiak's local learning rule
 # Relu/Sigmoid/tanh activation function
 # Update weight per sample
@@ -27,7 +28,7 @@ def get_act(act):
     linear = lambda x: x
     linear_deriv = lambda y: 1
     tanh = lambda x: np.tanh(x)
-    tanh_deriv = lambda y: 1.0 - np.tanh(y)**2
+    tanh_deriv = lambda y: 1.0 - np.tanh(y) ** 2
     if act == 'relu':
         return relu, relu_deriv
     elif act == 'sigmoid':
@@ -36,7 +37,8 @@ def get_act(act):
         return tanh, tanh_deriv
     return linear, linear_deriv
 
-def foldiak(eta=0.01, act=None):
+
+def foldiak(eta=0.01, act=None, inhi_strength=0.3):
     """A fully connected linear layer with Foldiak's rule."""
 
     def forward(x, **kwargs):
@@ -53,7 +55,9 @@ def foldiak(eta=0.01, act=None):
         q = kwargs['q']
         y1 = x.dot(w)
         y2 = y1.dot(q)
-        y = y1+y2
+        y = y1 + y2
+        # w2 = w.dot(np.linalg.inv(np.eye(q.shape[0]) - q))
+        # y = x.dot(w2)
         if act is not None:
             fwd, bak = get_act(act)
             y = fwd(y)
@@ -72,12 +76,15 @@ def foldiak(eta=0.01, act=None):
         """
         w = kwargs['w']
         q = kwargs['q']
-        dw = x.T.dot(y) - w * np.sum(y.T.dot(y) * np.eye(w.shape[1]), keepdims=1, axis=0).reshape(1,-1)
+        dw = x.T.dot(y) - w * np.sum(y.T.dot(y) * np.eye(w.shape[1]), keepdims=1, axis=0).reshape(1, -1)
         dq = -1 * np.tril(y.T.dot(y), -1)
-        
+        # _y = np.expand_dims(y, 1)
+        # y_ = np.expand_dims(y, 2)
+        # dq = -1 * eta * (_y * y_).mean(axis=0)
+
         w = w + eta * dw
-        q = q + 0.3 * eta * dq
-        
+        q = q + inhi_strength * eta * dq
+
         return {'w': w, 'q': q}
 
     return forward, update
